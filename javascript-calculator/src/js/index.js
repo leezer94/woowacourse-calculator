1202c1cadfd772508426b2f32e2e86c356b4baf7
leezer94 <2kunhee94@gmail.com>
2022-02-10 17:30:06 +0900

    Feat: 추가 디스플레이 숫자 길이 유효성 검사 기능
    계산기 스크린에 숫자 3 자리까지만 출력 가능하게하는 유효성 검사 추가

--- javascript-calculator/src/js/index.js
+++ javascript-calculator/src/js/index.js
@@ -3,23 +3,60 @@ class Calculator {
     this.$ = {
       total: document.querySelector('#total'),
       digits: document.querySelector('.digits'),
+      modifiers: document.querySelector('.modifiers'),
+      operations: document.querySelector('.operations'),
+      equalSign: document.querySelector('.equal-sign'),
     };
 
     this.bindEventListeners();
+    this.state = {
+      storage: 'array',
+      zero: 0,
+      operations: {
+        multiplication: 'X',
+        division: '/',
+        subtraction: '-',
+        addition: '+',
+      },
+      length: 2,
+      string: {
+        zero: '0',
+      },
+    };
   }
 
   onClickDigitBtn({ target }) {
     const digit = target.innerText;
 
-    if (this.$.total.innerText === '0') {
-      return (this.$.total.innerText = digit);
-    } else {
-      return (this.$.total.innerText += digit);
+    if (this.isValidLength()) {
+      if (this.$.total.innerText === this.state.string.zero) {
+        return (this.$.total.innerText = digit);
+      } else {
+        return (this.$.total.innerText += digit);
+      }
     }
+  }
+
+  isValidLength() {
+    let isValid = false;
+
+    if (this.$.total.innerText.length > this.state.length) {
+      isValid;
+    } else {
+      isValid = true;
+    }
+
+    return isValid;
   }
 
   bindEventListeners() {
     this.$.digits.addEventListener('click', this.onClickDigitBtn.bind(this));
+    this.$.modifiers.addEventListener('click', this.initDisplay.bind(this));
+    this.$.operations.addEventListener(
+      'click',
+      this.addFirstNumToStorage.bind(this)
+    );
+    this.$.equalSign.addEventListener('click', this.calculate.bind(this));
   }
 }
 
